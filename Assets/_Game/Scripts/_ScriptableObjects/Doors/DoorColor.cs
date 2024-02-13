bbc85e8311692e427b8e9d82e725763d60ae8bc4
Mikael Björke <651682+Mekyi@users.noreply.github.com>
2024-02-13 21:04:26 +0200

    Rename door scriptable menu contexts

--- Assets/_Game/Scripts/_ScriptableObjects/Doors/DoorColor.cs
+++ Assets/_Game/Scripts/_ScriptableObjects/Doors/DoorColor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Door Color", menuName = "Scriptable Objects/Doors/Door Color")]
+[CreateAssetMenu(fileName = "Door Color", menuName = "Scriptable Objects/Doors/New Door Color")]
 public class DoorColor : DoorTraitBase
 {
     [field: SerializeField]
--- Assets/_Game/Scripts/_ScriptableObjects/Doors/DoorShape.cs
+++ Assets/_Game/Scripts/_ScriptableObjects/Doors/DoorShape.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Door Shape", menuName = "Scriptable Objects/Doors/Door Shape")]
+[CreateAssetMenu(fileName = "Door Shape", menuName = "Scriptable Objects/Doors/New Door Shape")]
 public class DoorShape : DoorTraitBase
 {
     [field: SerializeField]
