e294a90df826507e87064918c4b8cb19a6845bb0
Mikael Björke <651682+Mekyi@users.noreply.github.com>
2024-02-18 09:35:16 +0200

    Basis for audio system

---
+++ Assets/_Game/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private List<EventInstance> _eventInstances;
+    private List<StudioEventEmitter> _studioEventEmitters;
+    private EventInstance _bgmEventInstance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        Instance = this;
+
+        _eventInstances = new List<EventInstance>();
+        _studioEventEmitters = new List<StudioEventEmitter>();
+    }
+
+    private void Start()
+    {
+        InitializaBGM(FMODEvents.Instance.BGM);
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPosition)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPosition);
+    }
+
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        _eventInstances.Add(eventInstance);
+
+        return eventInstance;
+    }
+
+    public StudioEventEmitter InitializeEventEmitter(EventReference eventReference, GameObject emitterGameObject)
+    {
+        StudioEventEmitter emitter = emitterGameObject.GetComponent<StudioEventEmitter>();
+        emitter.EventReference = eventReference;
+        _studioEventEmitters.Add(emitter);
+
+        return emitter;
+    }
+
+    private void InitializaBGM(EventReference bgmEventReference)
+    {
+        _bgmEventInstance = CreateInstance(bgmEventReference);
+        _bgmEventInstance.start();
+    }
+
+    private void OnDestroy()
+    {
+        CleanEventInstances();
+    }
+
+    private void CleanEventInstances()
+    {
+        foreach (var eventInstance in _eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+
+        foreach (var emitter in _studioEventEmitters)
+        {
+            emitter.Stop();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/FMODEvents.cs
@@ -0,0 +1,33 @@
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("BGM")]
+    [field: SerializeField]
+    public EventReference BGM { get; private set; }
+
+
+    [field: Header("SFX")]
+
+    [field: SerializeField]
+    public EventReference DoorOpen { get; private set; }
+
+    public static FMODEvents Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        Instance = this;
+    }
+}
